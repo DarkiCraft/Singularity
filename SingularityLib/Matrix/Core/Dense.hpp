624a84d637e7dcf94943f322c34e6c2f84dbf5e7
DarkiCraft <2005muhammadabdurrahman@proton.me>
2025-06-08 13:25:44 +0500

    Updated `DenseCore` to use `size_type` and `difference_type` plus clarity improvements

--- SingularityLib/Matrix/Core/Dense.hpp
+++ SingularityLib/Matrix/Core/Dense.hpp
@@ -14,31 +14,34 @@ namespace Sglty {
 template <typename _Tp, size_t _rows, size_t _cols, CoreOrdr _core_ordr>
 class DenseCore {
  public:
-  using size_traits = SizeTraits<_rows, _cols>;
-
   using type_traits = TypeTraits<_Tp>;
 
-  using core_traits = CoreTraits<CoreMode::Dense, _core_ordr>;
-
-  template <size_t _rebind_rows, size_t _rebind_cols>
-  using core_rebind =
-      DenseCore<_Tp, _rebind_rows, _rebind_cols, core_traits::core_ordr>;
-
-  using core_base = DenseCore<_Tp, 0, 0, core_traits::core_ordr>;
-
   using value_type      = typename type_traits::value_type;
+  using size_type       = typename type_traits::size_type;
   using allocator_type  = typename type_traits::allocator_type;
+  using difference_type = typename type_traits::difference_type;
   using reference       = typename type_traits::reference;
   using const_reference = typename type_traits::const_reference;
   using pointer         = typename type_traits::pointer;
   using const_pointer   = typename type_traits::const_pointer;
 
+  using size_traits = SizeTraits<_rows, _cols>;
+
+  using core_traits = CoreTraits<CoreMode::Dense, _core_ordr>;
+
+  template <size_type _rebind_rows, size_type _rebind_cols>
+  using core_rebind =
+      DenseCore<_Tp, _rebind_rows, _rebind_cols, core_traits::core_ordr>;
+
+  using core_base = DenseCore<_Tp, 0, 0, core_traits::core_ordr>;
+
   DenseCore() = default;
 
-  constexpr reference At(const size_t _row, const size_t _col) {
+  constexpr reference At(const size_type _row, const size_type _col) {
     return const_cast<reference>(std::as_const(*this).At(_row, _col));
   }
-  constexpr const_reference At(const size_t _row, const size_t _col) const {
+  constexpr const_reference At(const size_type _row,
+                               const size_type _col) const {
     return _m_Get(_row, _col);
   }
 
@@ -52,11 +55,12 @@ class DenseCore {
  private:
   std::array<_Tp, _rows * _cols> _m_data;
 
-  constexpr inline reference _m_Get(const size_t _row, const size_t _col) {
+  constexpr inline reference _m_Get(const size_type _row,
+                                    const size_type _col) {
     return const_cast<reference>(std::as_const(*this)._m_Get(_row, _col));
   }
-  constexpr inline const_reference _m_Get(const size_t _row,
-                                          const size_t _col) const {
+  constexpr inline const_reference _m_Get(const size_type _row,
+                                          const size_type _col) const {
     if (core_traits::core_ordr == CoreOrdr::RowMajor) {
       return _m_data[_row * _cols + _col];
     } else {
