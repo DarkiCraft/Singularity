73e3194d86bdee45568eb5a2ff0cfcb4c1b30055
DarkiCraft <2005muhammadabdurrahman@proton.me>
2025-06-08 13:16:13 +0500

    Updated for consistency

--- SingularityLib/Matrix/Traits/Type.hpp
+++ SingularityLib/Matrix/Traits/Type.hpp
@@ -17,16 +17,16 @@ namespace Sglty {
  * - `const_pointer`
  *
  * @tparam _Tp The element type.
- * @tparam Alloc The allocator type (default is `std::allocator<_Tp>`).
+ * @tparam _Alloc The allocator type (default is `std::allocator<_Tp>`).
  */
-template <typename _Tp, typename Alloc = std::allocator<_Tp>>
+template <typename _Tp, typename _Alloc = std::allocator<_Tp>>
 struct TypeTraits {
   using value_type      = _Tp;
-  using allocator_type  = Alloc;
+  using allocator_type  = _Alloc;
   using reference       = value_type&;
   using const_reference = const value_type&;
-  using pointer         = typename std::allocator_traits<Alloc>::pointer;
-  using const_pointer   = typename std::allocator_traits<Alloc>::const_pointer;
+  using pointer         = typename std::allocator_traits<_Alloc>::pointer;
+  using const_pointer   = typename std::allocator_traits<_Alloc>::const_pointer;
 };
 
 }  // namespace Sglty
