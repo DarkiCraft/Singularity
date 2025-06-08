ff0567e0ac238196291918f09454e37c3582a2da
DarkiCraft <2005muhammadabdurrahman@proton.me>
2025-06-08 13:24:40 +0500

    Added `size_type` and `difference_type` aliases in `TypeTraits` for better STL-style abstraction and easier future tweaks

--- SingularityLib/Matrix/Traits/Type.hpp
+++ SingularityLib/Matrix/Traits/Type.hpp
@@ -1,7 +1,11 @@
 #ifndef SINGULARITYLIB_MATRIX_TRAITS_TYPE_HPP
 #define SINGULARITYLIB_MATRIX_TRAITS_TYPE_HPP
 
+#include <cstddef>
 #include <memory>
+
+using std::ptrdiff_t;
+using std::size_t;
 
 namespace Sglty {
 
@@ -10,7 +14,9 @@ namespace Sglty {
  *
  * Defines the following type members:
  * - `value_type`
+ * - `size_type`
  * - `allocator_type`
+ * - `difference_type`
  * - `reference`
  * - `const_reference`
  * - `pointer`
@@ -19,10 +25,15 @@ namespace Sglty {
  * @tparam _Tp The element type.
  * @tparam _Alloc The allocator type (default is `std::allocator<_Tp>`).
  */
-template <typename _Tp, typename _Alloc = std::allocator<_Tp>>
+template <typename _Tp,
+          typename _size_type       = size_t,
+          typename _Alloc           = std::allocator<_Tp>,
+          typename _difference_type = ptrdiff_t>
 struct TypeTraits {
   using value_type      = _Tp;
+  using size_type       = _size_type;
   using allocator_type  = _Alloc;
+  using difference_type = _difference_type;
   using reference       = value_type&;
   using const_reference = const value_type&;
   using pointer         = typename std::allocator_traits<_Alloc>::pointer;
