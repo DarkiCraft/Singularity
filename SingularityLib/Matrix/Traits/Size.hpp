07c317b7a1c6486bacd0b0fe7ec4bb0b6e966dce
DarkiCraft <2005muhammadabdurrahman@proton.me>
2025-06-08 13:12:21 +0500

    Added Doxygen for `Matrix/Traits/Size.hpp`

--- SingularityLib/Matrix/Traits/Size.hpp
+++ SingularityLib/Matrix/Traits/Size.hpp
@@ -7,6 +7,16 @@ using std::size_t;
 
 namespace Sglty {
 
+/**
+ * @brief Trait to represent static matrix dimensions.
+ *
+ * Holds the following constant expressions:
+ * - `rows`
+ * - `cols`
+ *
+ * @tparam _rows Number of rows.
+ * @tparam _cols Number of columns.
+ */
 template <size_t _rows, size_t _cols>
 struct SizeTraits {
   static constexpr size_t rows = _rows;
